a6fe155ac6f9b25dc003d506c2be23c30756ecc2
Pádraig Ó Cinnéide <padraig.kennedy@gmail.com>
2023-10-14 11:25:09 -0700

    Improve naming of animations
    Fixes #6

--- Sources/SceneGen/Helpers/String+Extensions.swift
+++ Sources/SceneGen/Helpers/String+Extensions.swift
@@ -5,22 +5,37 @@
 
 import Foundation
 
+
 extension String {
     func swiftName() -> String {
         if self == uppercased() {
             lowercased()
         } else {
-            withFirstLetterLowercased()
+            lowercaseFirstLetter()
         }
     }
 
     func snakeToCamelcase() -> String {
-        split(separator: "_").map { String($0).swiftName() }.joined()
+        let parts = split(separator: "_")
+        
+        guard let firstItem = parts.first else {
+             return ""
+        }
+        
+        return String(firstItem).swiftName() +
+        parts.dropFirst().map { String($0).uppercaseFirstLetter() }.joined()
     }
 
-    private func withFirstLetterLowercased() -> String {
+    private func lowercaseFirstLetter() -> String {
         if let firstLetter = first {
             firstLetter.lowercased() + dropFirst()
+        } else {
+            self
+        }
+    }
+    private func uppercaseFirstLetter() -> String {
+        if let firstLetter = first {
+            firstLetter.uppercased() + dropFirst()
         } else {
             self
         }
